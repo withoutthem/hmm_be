3fa5b6a5a3ef9effe0f44a30db474225dff3e4fb
Victor Lee <withoutthem50@gmail.com>
2025-09-03 21:31:46 +0900

    TEST

--- src/main/java/com/hmm/cbui/domain/test/controller/TestController.java
+++ src/main/java/com/hmm/cbui/domain/test/controller/TestController.java
@@ -2,17 +2,17 @@
 package com.hmm.cbui.domain.test.controller;
 
 import org.springframework.http.ResponseEntity;
-
-import com.hmm.cbui.domain.livechat.dto.LiveChatMessageDto;
-import com.hmm.cbui.domain.livechat.service.LiveChatService;
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hmm.cbui.domain.livechat.dto.LiveChatMessageDto;
+import com.hmm.cbui.domain.livechat.service.LiveChatService;
+
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -29,7 +29,7 @@ public class TestController {
 
   @PostMapping("/broadcast/{roomId}")
   public Mono<ResponseEntity<Void>> testWebSocketBroadcast(
-          @PathVariable String roomId, @RequestBody LiveChatMessageDto message) {
+      @PathVariable String roomId, @RequestBody LiveChatMessageDto message) {
     // LiveChatService의 publish 메소드를 호출
     liveChatService.publish(roomId, message);
     return Mono.just(ResponseEntity.ok().build());
